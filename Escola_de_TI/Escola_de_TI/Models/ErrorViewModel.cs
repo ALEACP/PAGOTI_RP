b50b48ac48b6827d01971dc4040818e166c9231b
ALEACP <86522824+ALEACP@users.noreply.github.com>
2021-12-07 22:31:59 -0300

    Escola de TI
    Esboço do projeto Escola de TI, para primeiro sistema web

---
+++ Escola_de_TI/Escola_de_TI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Escola_de_TI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
